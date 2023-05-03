a017def84b40093434a5b5f3a1ab592583d32d9f
Ero <imjustero@gmail.com>
2023-05-03 17:56:51 +0200

    wip

--- src/AslHelp.Core/Exceptions/ThrowHelper.Throw.cs
+++ src/AslHelp.Core/Exceptions/ThrowHelper.Throw.cs
@@ -16,79 +16,169 @@ internal static partial class ThrowHelper
         /// </summary>
         /// <param name="paramName">The name of the parameter that caused the exception.</param>
         /// <param name="message">The error message that explains the reason for the exception.</param>
-        /// <exception cref="ArgumentException"></exception>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="ArgumentException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void Argument(string paramName, string message)
         {
             throw new ArgumentException(message, paramName);
         }
 
+        /// <summary>
+        ///     Throws an <see cref="ArgumentNullException"/> with the name of the parameter that causes this exception.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void ArgumentNull(string paramName)
         {
             throw new ArgumentNullException(paramName);
         }
 
+        /// <summary>
+        ///     Throws an <see cref="ArgumentNullException"/> with a specified error message
+        ///     and the name of the parameter that causes this exception.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void ArgumentNull(string paramName, string message)
         {
             throw new ArgumentNullException(paramName, message);
         }
 
+        /// <summary>
+        ///     Throws an <see cref="ArgumentOutOfRangeException"/> with a specified error message
+        ///     and the name of the parameter that causes this exception.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="ArgumentOutOfRangeException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void ArgumentOutOfRange(string paramName, string message)
         {
             throw new ArgumentOutOfRangeException(message, paramName);
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void DirectoryNotFound()
-        {
-            throw new DirectoryNotFoundException();
-        }
-
+        /// <summary>
+        ///     Throws a <see cref="DirectoryNotFoundException"/> with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="DirectoryNotFoundException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void DirectoryNotFound(string message)
         {
             throw new DirectoryNotFoundException(message);
         }
 
+        /// <summary>
+        ///     Throws a <see cref="FileNotFoundException"/> with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="FileNotFoundException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void FileNotFound(string message)
         {
             throw new FileNotFoundException(message);
         }
 
+        /// <summary>
+        ///     Throws a <see cref="FileNotFoundException"/> with a specified error message
+        ///     and the name of the file name that cannot be found.
+        /// </summary>
+        /// <param name="fileName">The full name of the file that cannot be found.</param>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="FileNotFoundException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void FileNotFound(string fileName, string message)
         {
             throw new FileNotFoundException(message, fileName);
         }
 
+        /// <summary>
+        ///     Throws a <see cref="FormatException"/> with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="FormatException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void Format(string message)
         {
             throw new FormatException(message);
         }
 
+        /// <summary>
+        ///     Throws an <see cref="InvalidDataException"/> with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="InvalidDataException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void InvalidData(string message)
         {
             throw new InvalidDataException(message);
         }
 
+        /// <summary>
+        ///     Throws an <see cref="InvalidOperationException"/> with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="InvalidOperationException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void InvalidOperation(string message)
         {
             throw new InvalidOperationException(message);
         }
 
+        /// <summary>
+        ///     Throws a <see cref="KeyNotFoundException"/> with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="KeyNotFoundException"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void KeyNotFound(string message)
         {
             throw new KeyNotFoundException(message);
         }
 
+        /// <summary>
+        ///     Throws a <see cref="Win32Exception"/>.
+        /// </summary>
+        /// <remarks>
+        ///     This method does not return and is not inlined to improve codegen of cold paths.
+        /// </remarks>
+        /// <exception cref="Win32Exception"/>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void Win32()
         {
--- src/AslHelp.Core/Extensions/ArrayPoolExtensions.cs
+++ src/AslHelp.Core/Extensions/ArrayPoolExtensions.cs
@@ -2,13 +2,32 @@
 
 namespace AslHelp.Core.Extensions;
 
+/// <summary>
+///     The <see cref="ArrayPoolExtensions"/> class
+///     provides useful extension methods for the <see cref="ArrayPool{T}"/> class.
+/// </summary>
 internal static class ArrayPoolExtensions
 {
-    public static T[] Rent<T>(int length)
+    /// <summary>
+    ///     Retrieves a buffer that is at least the requested length.
+    /// </summary>
+    /// <typeparam name="T">The type of the objects that are in the resource pool.</typeparam>
+    /// <param name="minimumLength">The minimum length of the array.</param>
+    /// <returns>
+    ///     An array of type <typeparamref name="T"/>[] that is at least <paramref name="minimumLength"/> in length.
+    /// </returns>
+    public static T[] Rent<T>(int minimumLength)
     {
-        return ArrayPool<T>.Shared.Rent(length);
+        return ArrayPool<T>.Shared.Rent(minimumLength);
     }
 
+    /// <summary>
+    ///     If <paramref name="array"/> is not <see langword="null"/>,
+    ///     returns an array to the pool that was previously obtained using the <see cref="ArrayPool{T}.Rent(int)"/>
+    ///     method on the same <see cref="ArrayPool{T}"/> instance.
+    /// </summary>
+    /// <typeparam name="T">The type of the objects that are in the resource pool.</typeparam>
+    /// <param name="array">The buffer to return to the pool.</param>
     public static void Return<T>(T[] array)
     {
         if (array is not null)
